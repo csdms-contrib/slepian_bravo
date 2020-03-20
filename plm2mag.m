ab72d535cd39d33d27d4b438ffde619c4f43aff7
Frederik J. Simons <fjsimons@gmail.com>
2020-03-20 00:00:11 -0400

    Performs conversion of geomagnetic potential coeffficients

--- plm2mag.m
+++ plm2mag.m
@@ -13,8 +13,8 @@ function lmcosip=plm2mag(lmcosi,r,a,wat,schm)
 % r             Requested radius for the input, in m [default: a]
 % a             Reference radius for the output [default: 6371200]
 % wat           1 output is radial-component field [default]
-%               2 output is colatitudinal tangential-component field [default]
-%               3 output is longitudinal tangential-component field [default]
+%               2 output is colatitudinal tangential-component field [not done yet]
+%               3 output is longitudinal tangential-component field [not done yet]
 % schm          1 change normalization from being appropriate for expansion
 %                 into Schmidt semi-normalized harmonics to being
 %                 appropriate for expansion into 4pi-normalized ones [default] 
@@ -25,8 +25,8 @@ function lmcosip=plm2mag(lmcosi,r,a,wat,schm)
 % lmcosip       [l m Ccos Csin] degrees, order, cos/sin coefficients of output
 %
 % Written by by fjsimons-at-alum.mit.edu, 03/05/2009
-% Modified by Jarno Saarimaki, 07/01/2011
-% Last modified by fjsimons-at-alum.mit.edu, 05/04/2018
+% Last modified by Jarno Saarimaki, 07/01/2011
+% Last modified by fjsimons-at-alum.mit.edu, 03/20/2020
 
 % Deafult input values
 defval('a',6371200)
@@ -34,27 +34,27 @@ defval('r',a)
 defval('wat',1)
 defval('schm',1)
 
-% Get the spherical harmonic degree
+% Get the spherical harmonic degrees
 el=lmcosi(:,1);
 
 % Precalculate a certain upward-continuation factor
 arl2=(a/r).^(el+2);
 
-if schm == 1
+if schm==1
   % Convert coefficients from being expanded into Schmidt semi-normalized
   % to being expanded into 4pi-normalized real spherical harmonics
   lmcosi(:,3:4)=lmcosi(:,3:4)./sqrt(2*[el el]+1);
 end
 
 % Perform the upward or downward continuation portion first
-lmcosi(:,3:4)=lmcosi(:,3:4).*arl2*[1 1];
+lmcosi(:,3:4)=lmcosi(:,3:4).*(arl2*[1 1]);
 
 % Convert to other things
 switch wat
  case 1
   % [Blakely 1995 p. 169 after eq. (8.20)]
   disp('Calculating radial field from potential')
-  lmcosip(:,3:4)=lmcosi(:,3:4).*(el+1)*[1 1];
+  lmcosip(:,3:4)=lmcosi(:,3:4).*((el+1)*[1 1]);
  case 2
   % Kono 
   disp('Calculating colatitudinal tangential field from potential')
